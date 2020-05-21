fd09ee7d5b741e00b8a319c077f7b0ed4f4e776d
Arek <arkadiusz.wisniewski92@gmail.com>
2020-05-21 16:41:57 +0200

    dodana opcja nr 3

--- api.py
+++ api.py
@@ -62,11 +62,34 @@ def currency_exchange():
             break
 
 
+def rates_over_time():
+    response = get_nbp_rates("https://api.nbp.pl/api/exchangerates/tables/a/")
+    currencies = get_all_available_currencies(response)
+    print(
+        f"Dane dla jakiej waluty (z poniżej wymienionych) chcesz sprawdzić?\n{', '.join(str(elem) for elem in currencies)}")
+    code = input("Trzy literowy kod waluty z lity powyżej: ")
+    print("Od kiedy?")
+    year_start = input("Rok (w formacie RRRR): ")
+    month_start = input("Miesiąc (w formacie MM): ")
+    day_start = input("Dzień (w formacie DD): ")
+    print("Do kiedy?")
+    year_end = input("Rok (w formacie RRRR): ")
+    month_end = input("Miesiąc (w formacie MM): ")
+    day_end = input("Dzień (w formacie DD): ")
+    response = get_nbp_rates(
+        f"http://api.nbp.pl/api/exchangerates/rates/a/{code}/{year_start}-{month_start}-{day_start}/{year_end}-{month_end}-{day_end}/")
+    n = 0
+    for elem in response["rates"]:
+        print("Data:", response["rates"][n]["effectiveDate"], ":", response["rates"][n]["mid"])
+        n = n + 1
+
+
 while True:
     print("\n>>\tMenu główne\t<<")
     print("0 - Zamknij program")
     print("1 - Przelicznik walutowy")
     print("2 - Dzisiejsza wartość złota")
+    print("3 - Wartość waluty w wybranym okresie")
     print("*Wszystkie dane są na podstawie notowań NBP.")
     print(">>\t\t\t\t<<")
     menu_option = input("W czym Ci mogę dzisiaj pomóc?\nWpisz numer z listy powyżej. ")
@@ -74,5 +97,7 @@ while True:
         currency_exchange()
     elif menu_option == "2":
         gold_today()
+    elif menu_option == "3":
+        rates_over_time()
     elif menu_option == "0":
         break
